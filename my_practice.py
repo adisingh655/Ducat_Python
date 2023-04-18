12f87563996f36adba30b60624a5cd90ad7480aa
NARENDRA SHARMA <adisingh655@gmail.com>
2023-04-18 13:43:33 +0530

    commited

---
+++ my_practice.py
@@ -0,0 +1,59 @@
+tuple=(1,2,3,4)
+print(tuple)
+
+#c="krishna"
+#print(float(c))
+
+a=31
+print(str(31))
+print(int(31))
+
+d="10k"
+e=d[0:2]
+print(e,(type(e)))
+print(int(e))
+
+z="10.0"
+x=float(z)
+print(x)
+c=int(x)
+print(c,type(c))
+
+c,d=10,20
+print("c",c)
+print("d",d)
+d,c=10,20
+print("c",c)
+print("d",d)
+
+l=10
+m=20
+temp=l+m
+print(temp)
+l=temp-l
+m=temp-m
+print("l",l)
+print("m",m)
+
+a=int(input("enter the age: "))
+if (a>=18):
+    print("Requirements met")
+else:
+    print("Age is less than 18")
+
+
+#num1= int(input("Enter the first values"))
+#num2= int(input("Enter the second value:"))
+#opr= input("select the operator +"/")
+#if opr== "+":
+
+#   print(num1+num2)
+#    if num1 < num2 :
+#        print("numi is less than num2")
+#    elif num1 > num2: 
+#        print("numi is greater than num2")
+#    elif opr == "-" 
+#   elif num1 > num2 : 
+#        print("numi is greater than num2")
+#    elif num1 < num2:
+#        print("numi is less than num")
